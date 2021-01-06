a706cc36a29a6989e30f2ae1a3dd1241e553a017
Lucky <lucky091588@gmail.com>
2021-01-06 15:24:52 +0800

    Add doPrimeUpdates function base on docs

--- libraries/Kimai/Remote/Database/Civicrm.php
+++ libraries/Kimai/Remote/Database/Civicrm.php
@@ -57,18 +57,176 @@ class Kimai_Remote_Database_Civicrm extends Kimai_Remote_Database
     }
 
     /**
-     * create exp entry
+     * Update server_prefix_timeSheet table
+     * Create server_prefix_civicrm_queue and server_prefixcivicrm_timesheet_ever table
      *
-     * @param array $data
-     * @return int
+     * @return array
      */
     public function doPrimeUpdates()
     {
-        $query = 'select database()';
+        $civicrmTimesheetEver = $this->getCivicrmTimesheetEver();
+        $civicrmQueue = $this->getCivicrmQueue();
+        $timeSheetTable = $this->getTimeSheetTable();
+        $columnToInsert = 'modified';
+
+        // Add default messages for the tables, this will appear if tables are already created.
+        $result[$civicrmTimesheetEver] = "{$civicrmTimesheetEver} table is already created.";
+        $result[$civicrmQueue] = "{$civicrmQueue} table is already created.";
+        $result[$timeSheetTable] = "{$timeSheetTable} table has already a `modified` column.";
+
+        // Check table if already exist
+        if (!$this->checkTableExist($civicrmTimesheetEver)) {
+            // if table doesn't exist, add function to create table and see if it succeed
+            $result[$civicrmTimesheetEver] = $this->addCivicrmTimesheetEver();
+        }
+
+        // Check table if already exist
+        if (!$this->checkTableExist($civicrmQueue)) {
+            // if table doesn't exist, add function to create table and see if it succeed
+            $result[$civicrmQueue] = $this->addCivicrmQueue();
+        }
+
+        // Check column if already exist in a table
+        if (!$this->checkColumnExist($timeSheetTable, $columnToInsert)) {
+            // if column doesn't exist in a table, add function to insert column table and see if it succeed
+            $result[$timeSheetTable] = $this->addColumnInTable($timeSheetTable, $columnToInsert);
+        }
+
+        // Return result to the primeUpdates in API
+        return $result;
+    }
+
+    /**
+     * @return string table name including prefix
+     */
+    public function getCivicrmTimesheetEver()
+    {
+        return $this->kga['server_prefix'] . 'civicrm_timesheet_ever';
+    }
+
+    /**
+     * @return string table name including prefix
+     */
+    public function getCivicrmQueue()
+    {
+        return $this->kga['server_prefix'] . 'civicrm_queue';
+    }
+
+    /**
+     * This custom function will check if table is already exist
+     * @param $tableName
+     * @return array
+     */
+    public function checkTableExist($tableName)
+    {
+        $query = 'SELECT * FROM ' . $tableName;
+        $result = $this->conn->Query($query);
+
+        return $result;
+    }
+
+    /**
+     * This custom function will check if column is already exist in a table
+     * @param $tableName
+     * @param $columnName
+     * @return array
+     */
+    public function checkColumnExist($tableName, $columnName)
+    {
+        $query = $query = "SELECT COLUMN_NAME
+                FROM INFORMATION_SCHEMA.COLUMNS
+                WHERE table_name = '$tableName'
+                AND column_name LIKE '$columnName'";
 
         $this->conn->Query($query);
 
         return $this->conn->RowArray(0, MYSQLI_ASSOC);
+    }
+
+
+    /**
+     * Create custom table and return success or fail message
+     *
+     * @return string
+     */
+    public function addCivicrmTimesheetEver()
+    {
+        $query = "CREATE TABLE `{$this->getCivicrmTimesheetEver()}` (
+            `timeEntryID` int(11) NOT NULL COMMENT 'pseudo fk to civicrm_timeSheet.timeEntryID',
+            `delete_timestamp` timestamp NULL DEFAULT NULL COMMENT 'timestamp when civicrm_timeSheet.timeEntryID was discovered to be deleted, or NULL if never deleted',
+            PRIMARY KEY (`timeEntryID`)
+        )";
+
+        $success = $this->conn->Query($query);
+
+        if (!$success) {
+            return "Failed to create {$this->getCivicrmTimesheetEver()} table";
+        }
+
+        return "{$this->getCivicrmTimesheetEver()} table is created.";
+    }
+
+    /**
+     * Create custom table and return success or fail message
+     *
+     * @return string
+     */
+    public function addCivicrmQueue()
+    {
+        $query = "CREATE TABLE `{$this->getCivicrmQueue()}` (
+            `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            `confirmed` timestamp NULL DEFAULT NULL COMMENT 'NULL if never confirmed, or timestamp when message was confirmed as received and processed by civicrm.',
+            `action` text NOT NULL COMMENT 'update or delete',
+            `timeEntryID` int(10) NOT NULL,
+            `start` int(10) NOT NULL default '0',
+            `end` int(10) NOT NULL default '0',
+            `duration` int(6) NOT NULL default '0',
+            `userID` int(10) NOT NULL,
+            `projectID` int(10) NOT NULL,
+            `activityID` int(10) NOT NULL,
+            `description` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL,
+            `comment` TEXT NULL DEFAULT NULL,
+            `commentType` TINYINT(1) NOT NULL DEFAULT '0',
+            `cleared` TINYINT(1) NOT NULL DEFAULT '0',
+            `location` VARCHAR(50),
+            `trackingNumber` varchar(30),
+            `rate` DECIMAL( 10, 2 ) NOT NULL DEFAULT '0',
+            `fixedRate` DECIMAL( 10, 2 ) DEFAULT NULL,
+            `budget` DECIMAL( 10, 2 ) NULL,
+            `approved` DECIMAL( 10, 2 ) NULL,
+            `statusID` SMALLINT NOT NULL,
+            `billable` TINYINT NULL,
+            INDEX ( `userID` ),
+            INDEX ( `projectID` ),
+            INDEX ( `activityID` )
+        )";
+
+        $success = $this->conn->Query($query);
+
+        if (!$success) {
+            return "Failed to create {$this->getCivicrmQueue()} table";
+        }
+
+        return "{$this->getCivicrmQueue()} table is created.";
+    }
+
+    /**
+     * Add a column in an existing table and return success or fail message
+     * @param $tableName
+     * @param $columnName
+     * @return string
+     */
+    public function addColumnInTable($tableName, $columnName)
+    {
+        $query = "ALTER TABLE {$tableName} ADD {$columnName} timestamp NULL";
+
+        $success = $this->conn->Query($query);
+
+        if (!$success) {
+            return "Failed to add $columnName column in $tableName";
+        }
+
+        return "$columnName is added as a column in $tableName";
     }
 
 }
