41b80bc67a69b837ed18072d84888807fd93c85b
lucky091588 <lucky091588@gmail.com>
2021-04-25 07:03:09 +0800

    Add projectId param in getQueuedData function

--- libraries/Kimai/Remote/Database/Civicrm.php
+++ libraries/Kimai/Remote/Database/Civicrm.php
@@ -308,7 +308,8 @@ class Kimai_Remote_Database_Civicrm extends Kimai_Remote_Database
     public function getQueuedData()
     {
         $limit = $this->sessionParams['limit'] ?? 25;
-        $query = "SELECT * FROM `{$this->getCivicrmQueue()}` WHERE `confirmed` IS NULL ORDER BY modified DESC LIMIT {$limit}";
+        $projectId = $this->sessionParams['projectId'];
+        $query = "SELECT * FROM `{$this->getCivicrmQueue()}` WHERE `confirmed` IS NULL AND `projectID` = {$projectId} ORDER BY modified DESC LIMIT {$limit}";
         $this->conn->Query($query);
         $queuedData['queued_data'] = $this->conn->RecordsArray(MYSQLI_ASSOC);
 
